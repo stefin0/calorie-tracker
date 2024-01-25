93a89a853dc74cf9a95a8c3021eac895b77ed55e
stefin <stefinracho@gmail.com>
2024-01-25 03:35:09 -0800

    calorie ring created

--- src/components/Budget/Budget.jsx
+++ src/components/Budget/Budget.jsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import Style from "./Budget.module.css";
+
 function Budget() {
-  return <h1>Budget</h1>;
+  const [number, setNumber] = useState(() => {
+    return JSON.parse(localStorage.getItem("number")) || "0";
+  });
+  const [input, setInput] = useState();
+
+  function handleSetInput(e) {
+    setInput(e.target.value);
+  }
+
+  function handleSetNumber() {
+    setNumber(input);
+  }
+
+  useEffect(() => {
+    localStorage.setItem("number", JSON.stringify(number));
+  }, [number])
+
+  return (
+    <>
+      {/* RING */}
+      <div
+        className={Style.ring}
+        style={{
+          background: `conic-gradient(black ${number * 3.6}deg, grey 0deg)`,
+        }}
+      >
+        <span className={Style.pValue}>{number}%</span>
+      </div>
+      <input type="number" onChange={handleSetInput} />
+      <button onClick={handleSetNumber}>Confirm</button>
+    </>
+  );
 }
 
 export default Budget;
